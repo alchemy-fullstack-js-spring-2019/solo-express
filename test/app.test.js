5c995ac205dec2f1d03c544b09e42b599f50bd76
mcnadams <mcneilbo@oregonstate.edu>
2019-04-15 10:52:52 -0700

    post, put, get tested and done

--- test/app.test.js
+++ test/app.test.js
@@ -7,49 +7,94 @@ describe('app routes', () => {
     beforeAll(done => {
         mkdirp('./data/tweets', done);
     });
-    afterAll(() => {
+    afterEach(() => {
         return Tweet.drop();
     });
 
-    // it('can create a new tweet', () => {
-    //     return request(app)
-    //         .post('/tweets')
-    //         .send({
-    //             handle: 'bonnie',
-    //             body: 'my first tweet'
-    //         })
-    //         .then(res => {
-    //             expect(res.body).toEqual({
-    //                 handle: 'bonnie',
-    //                 body: 'my first tweet',
-    //                 _id: expect.any(String)
-    //             });
-    //         });
-    // });
-
-    it('can get all tweets', () => {
-        return request(app)
-            .get('/tweets')
-            .then(res => {
-                console.log(res.body);
-                //expect res.body to equal created tweets
-            });
-    });
-
-    it('can get a tweet by id', () => {
+    it('can create a new tweet', () => {
         return request(app)
             .post('/tweets')
             .send({
                 handle: 'bonnie',
                 body: 'my first tweet'
             })
-            .then(res => res.body._id)
-            .then(id => {
-                console.log('id', id);
+            .then(res => {
+                expect(res.body).toEqual({
+                    handle: 'bonnie',
+                    body: 'my first tweet',
+                    _id: expect.any(String)
+                });
             });
     });
 
-    
+    it('can get all tweets', () => {
+        return Tweet
+            .create({ handle: 'bonnie', body: 'tweet' })
+            .then(() => {
+                return request(app)
+                    .get('/tweets');
+            })
+            .then(res => {
+                expect(res.body).toHaveLength(1);
+            });
+    });
 
+    // this test technically is testing both post and get,
+    // but I like it for practice so I'll keep it and make a 
+    // new get test
+    it('can post a tween and then get it by id', () => {
+        return request(app)
+            .post('/tweets')
+            .send({
+                handle: 'bonnie',
+                body: 'my second tweet'
+            })
+            .then(res => res.body._id)
+            .then(id => {
+                return request(app)
+                    .get(`/tweets/${id}`);
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    handle: 'bonnie',
+                    body: 'my second tweet',
+                    _id: expect.any(String)
+                });
+            });
+    });
+
+    it('can get a tweet by id', () => {
+        return Tweet.create({ handle: 'barry', body: 'meow'})
+            .then(createdTweet => {
+                return request(app)
+                    .get(`/tweets/${createdTweet._id}`);
+            })
+            .then(res => {
+                expect(res.body).toEqual({ 
+                    handle: 'barry', 
+                    body: 'meow',
+                    _id: expect.any(String)
+                });
+            });
+    });
+
+    it('can update a tweet by id', () => {
+        return Tweet.create({ handle: 'b', body: 'drivel' })
+            .then(tweet => {
+                return request(app)
+                    .put(`/tweets/${tweet._id}`)
+                    .send({
+                        handle: 'b',
+                        body: 'something more interesting'
+                    });
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    handle: 'b',
+                    body: 'something more interesting',
+                    _id: expect.any(String)
+                });
+            });
+    });
 
 });
