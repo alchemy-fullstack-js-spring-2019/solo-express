4249dcf650abf29cbf2adeb928a490f2c74ea12f
Cheri Portman <cheriportman@hotmail.com>
2019-04-15 10:51:19 -0700

    get(all) test passes

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,12 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const Tweet = require('../lib/models/Tweet');
 
 describe('app routes', () => {
+  afterEach(() => {
+    return Tweet.drop();
+  });
+
   it('can create a new tweet', () => {
     return request(app)
       .post('/tweets')
@@ -15,6 +20,18 @@ describe('app routes', () => {
           body: 'first tweet in my app', 
           _id: expect.any(String)
         });
+      });
+  });
+
+  it('can get a list of tweets', () => {
+    return Tweet
+      .create({ handle: 'ryan', body: 'my tweet' })
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
       });
   });
 });
