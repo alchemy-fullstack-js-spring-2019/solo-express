1ca7bb559b9247d4df0f5d36c6d462efed0e325c
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-04-15 10:15:13 -0700

    working on first test

---
+++ tests/app.test.js
@@ -0,0 +1,19 @@
+const app = require('../lib/app');
+
+describe('app routes', () => {
+  it('can make a new tweet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({ 
+        handle: 'meg',
+        body: 'whatever'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'meg',
+          body: 'whatever',
+          _id: expect.any(String)
+        });
+      });
+  });
+});
