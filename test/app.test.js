ecfdc159e33c4047c67512c3102140a5a88f82a4
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-15 13:10:14 -0700

    passing all tests but update tweet

--- test/app.test.js
+++ test/app.test.js
@@ -21,4 +21,60 @@ describe('app routes', () => {
                 });
             });
     });
+    it('returns a list of tweets', () => {
+        return Tweet.create({ handle: 'Ryan', body: 'Dope Tweet Today' })
+            .then(() => {
+                return request(app)
+                    .get('/tweets')
+                    .then(response => {
+                        expect(response.body).toHaveLength(1);
+                    });
+            });
+    });
+    it('can get a tweet by id', () => {
+        return Tweet.create({ handle: 'Ryan', body: 'GETTING tweets by ID Today' })
+            .then(createdTweet => {
+                return request(app)
+                    .get(`/tweets/${createdTweet._id}`)
+                    .then(response => {
+                        expect(response.body).toEqual({
+                            handle: 'Ryan',
+                            body: 'GETTING tweets by ID Today',
+                            _id: expect.any(String)
+                        });
+                    });
+            });
+    });
+    it('can update a tweet by its id', () => {
+        return Tweet.create({ handle: 'Colin', body: 'this tweet will be updated' })
+            .then(updatedTweet => {
+                console.log(updatedTweet._id);
+                return request(app)
+                    .put(`/tweets/${updatedTweet._id}`)
+                    .send({
+                        handle: 'Colin',
+                        body: 'This was updated'
+                    });
+            })
+            .then(response => {
+                console.log(response.body);
+                expect(response.body).toEqual({
+                    handle: 'Colin',
+                    body: 'This was updated',
+                    _id: expect.any(String)
+                });
+            });
+    });
+    it('can delete a tweet by id', () => {
+        return Tweet.create({ handle: 'Colin', body: 'this tweet will be deleted' })
+            .then(tweet => {
+                return request(app)
+                    .delete(`/tweets/${tweet._id}`);
+            })
+            .then(response => {
+                expect(response.body).toEqual({
+                    deleted: 1
+                });
+            });
+    });
 });
