7953eb794c5025141b87ca3fa7d10b8916bf8f84
Ben Parisot <me@benparisot.com>
2019-04-15 14:07:44 -0700

    write and pass tests for update and delete routes and pass

--- tests/tag.test.js
+++ tests/tag.test.js
@@ -33,9 +33,9 @@ describe('tag router', () => {
     });
 
     it('finds a tag by id', () => {
-        Tag
+        return Tag
         .create({ tag: '#clients' })
-        .then(() => {
+        .then(tag => {
             return request(app)
             .get(`/tags/${tag._id}`);
         })
@@ -45,5 +45,35 @@ describe('tag router', () => {
                 _id: expect.any(String)
             });
         });
+    });
+
+    it('updates a tag by id', () => {
+        return Tag.create({ tag: '#fried' })
+        .then(tag => {
+            return request(app)
+            .put(`/tags/${tag._id}`)
+            .send({
+                tag: '#friend'
+            });
+        })
+        .then(results => {
+            expect(results.body).toEqual({
+                tag: '#friend',
+                _id: expect.any(String)
+            });
+        });
+    });
+
+    it('deletes a tag by id', () => {
+      return Tag.create({ tag: '#butts' })
+      .then(tag => {
+          return request(app)
+          .delete(`/tags/${tag._id}`);
+      })  
+      .then(result => {
+          expect(result.body).toEqual({
+              deleted: 1
+          });
+      });
     });
 });
