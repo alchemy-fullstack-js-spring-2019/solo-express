aae289165fc53c2c0a101628dca5d4ed809872ef
Peter Kotowski <bighoopee@gmail.com>
2019-04-15 11:21:10 -0700

    fixing tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,17 +1,22 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const Tweet = require('../lib/models/Tweets');
 
 describe('app routes', () => {
-  it('can create a tweet', () => {
+  afterEach(() => {
+    return Tweet.drop();
+  });
+
+  it('can create a new tweet', () => {
     return request(app)
       .post('./tweets')
       .send({
-        handle: 'gilagamesh',
+        handle: 'gilgamesh',
         body: 'enkidu ?'
       })
       .then(res => {
         expect(res.body).toEqual({
-          handle: 'gilagamesh',
+          handle: 'gilgamesh',
           body: 'enkidu ?',
           _id: expect.any(String)
         });
