9fc13b4790c29ed88fef68cb3426637db2d5171e
Dave Cornelius <d_cornelius94@yahoo.com>
2019-04-15 14:37:54 -0700

    update/delete by id tests passed

--- test/app.test.js
+++ test/app.test.js
@@ -48,4 +48,36 @@ describe('app routes', () => {
         });
       });
   });
+
+  it('updates a tweet by the id', () => {
+    return Tweet.create({ handle: 'dave', body: 'a good tweet' })
+      .then(tweet => {
+        return request(app)
+          .put(`/tweets/${tweet._id}`)
+          .send({
+            handle: 'dave',
+            body: 'a good tweet'
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'dave',
+          body: 'a good tweet',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('deletes a tweet by the id', () => {
+    return Tweet.create({ handle: 'dave', body: 'a tweet' })
+      .then(tweet => {
+        return request(app)
+          .delete(`/tweets/${tweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          deleted: 1
+        });
+      });
+  });
 });
