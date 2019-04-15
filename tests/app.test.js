e828e7b48dab07cf6b6cf9a55cb4be3d5ec8c663
Ben Parisot <me@benparisot.com>
2019-04-15 11:13:12 -0700

    write and pass find all tweets test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,11 @@
 const app = require('../lib/app');
 const request = require('supertest');
+const Tweet = require('../lib/models/Tweet');
 
 describe('tweet router', () => {
+    afterEach(() => {
+        return Tweet.drop();
+      });
     it('creates a new tweet', () => {
         return request(app)
         .post('/tweets')
@@ -15,6 +19,18 @@ describe('tweet router', () => {
                 body: 'hello there',
                 _id: expect.any(String)
             });
+        });
+    });
+
+    it('finds all tweets', () => {
+        return Tweet
+        .create({ handle: 'ben', body: 'hello' })
+        .then(() => {
+            return request(app)
+            .get('/tweets');
+        })
+        .then(tweets => {
+            expect(tweets.body).toHaveLength(1);
         });
     });
 
