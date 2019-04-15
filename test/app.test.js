a99a223398b70f1ba9363213d800a8ce2a6d08b8
Mallarie <mallariemilano@gmail.com>
2019-04-15 10:07:23 -0700

    failing test

---
+++ test/app.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('app routes', () => {
+  it('can create a new tweet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        handle: 'Mal',
+        body: 'My first tweet'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'Mal',
+          body: 'My first tweet',
+          _id: expect.any(String)
+        });
+      });
+  });
+});
