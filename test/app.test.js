624c0f7023e28d92cc641a9f2e62779f5b862066
Mallarie <mallariemilano@gmail.com>
2019-04-15 14:39:30 -0700

    can get some tweets test passing

--- test/app.test.js
+++ test/app.test.js
@@ -3,7 +3,6 @@ const app = require('../lib/app');
 const Tweet = require('../lib/models/Tweet');
 
 describe('app routes', () => {
-
   afterEach(() => {
     return Tweet.drop();
   });
@@ -21,6 +20,18 @@ describe('app routes', () => {
           body: 'My first tweet',
           _id: expect.any(String)
         });
+      });
+  });
+
+  it('it can get some tweets', () => {
+    return Tweet
+      .create({ handle: 'Mal', body: 'my tweet' })
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
       });
   });
 });
