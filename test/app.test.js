7ee9931b20199e28c548ba7a153d513778bd3a94
Tommy Tran <tranttommy95@gmail.com>
2019-04-15 10:51:47 -0700

    GET by ID test passed

--- test/app.test.js
+++ test/app.test.js
@@ -1,15 +1,50 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const Tweet = require('../lib/models/Tweet');
 
 describe('Tweet route', () => {
+  afterEach(() => {
+    return Tweet.drop();
+  });
+
   it('responds with a tweet object on POST', () => {
     return request(app)
       .post('/tweets')
-      .send({ handle: 'Tommy', text: 'First tweet' })
-      .then(response => {
-        expect(response.body).toEqual({
+      .send({ handle: 'Tommy', body: 'First tweet' })
+      .then(res => {
+        expect(res.body).toEqual({
           handle: 'Tommy',
-          text: 'First tweet',
+          body: 'First tweet',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('responds with all tweets on GET', () => {
+    return request(app)
+      .post('/tweets')
+      .send({ handle: 'Tommy', body: 'First tweet' })
+      .then(() => request(app).get('/tweets'))
+      .then(res => {
+        expect(res.body).toEqual(expect.any(Array));
+        expect(res.body).toHaveLength(1);
+        expect(res.body).toContainEqual({
+          handle: 'Tommy',
+          body: 'First tweet',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('responds with a tweet by ID on GET', () => {
+    return request(app)
+      .post('/tweets')
+      .send({ handle: 'Tommy', body: 'First tweet' })
+      .then(res => request(app).get(`/tweets/${res.body._id}`))
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'Tommy',
+          body: 'First tweet',
           _id: expect.any(String)
         });
       });
