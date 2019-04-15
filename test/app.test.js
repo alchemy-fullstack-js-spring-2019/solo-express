e833a8c19ad58394fadf4769953e6b1e2ecf94fe
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-04-15 13:38:44 -0700

    get by ID function passing

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,11 @@
 const request = require('supertest');
 const app = require('../lib/app.js');
+const TweetStore = require('../models/Tweet.js');
 
 describe('testing routes', () => {
+  afterEach(() => {
+    return TweetStore.drop();
+  });
   it('can create a new tweet', () => {
     return request(app)
       .post('/tweets')
@@ -17,4 +21,33 @@ describe('testing routes', () => {
         });
       });
   });
+
+  it('returns an array of all tweet objects', () => {
+    return TweetStore
+      .create({ twitterHandle: 'introooomode', tweet: 'heyhi' })
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(tweetsArray => {
+        expect(tweetsArray.body).toHaveLength(1);
+      });
+  });
+  it('can get a tweet by id', () => {
+    return TweetStore
+      .create({ twitterHandle: 'introooomode', tweet: 'heyhi' })
+      .then(createdTweet => {
+        return request(app) //what exactly is this even doing? Why wouldn't it be TweetStore?
+          .get(`/tweets/${createdTweet._id}`);
+      })
+      .then(tweet => {
+        expect(tweet.body).toEqual({ 
+          twitterHandle: 'introooomode',
+          tweet: 'heyhi',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+
 });
