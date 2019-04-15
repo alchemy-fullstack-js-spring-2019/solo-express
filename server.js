141df6fada4bfc55c4097ee33f62a254c2ddef68
dreamer <wildetangeant@gmail.com>
2019-04-15 13:36:27 -0700

    added querysearch

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 app.get('/', function (req, res) {
-    res.send('Hello World!')
-  });
+  res.send('Hello World!')
+});
 
 app.listen(7890, () => {
   console.log('started on port 7890');
--- test/app.test.js
+++ test/app.test.js
@@ -3,19 +3,56 @@ const app = require('../lib/app');
 const Tweet = require('../lib/models/Tweet');
 
 describe('app routes', () => {
-    it('can create a new tweeet', () => {
+  afterEach(() => {
+    return Tweet.drop();
+  });
+
+  //CRUD
+  //CREATE
+  it('can create a new tweeet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        handle: 'human001',
+        body: 'Twit twat'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'human001',
+          body: 'Twit twat',
+          _id : expect.any(String)
+        });
+      });
+  });
+
+  //GetALL
+  it('can get a list of tweets', () => {
+    return Tweet
+      .create({ handle: 'human 002', body: 'Where lies the strangling fruit that came from the hand of the sinner'})
+      .then(() => {
         return request(app)
-            .post('/tweets')
-            .send({
-                handle: 'human001',
-                body: 'Twit twat'
-            })
-            .then(res => {
-                expect(res.body).toEqual({
-                    handle: 'human001',
-                    body: 'Twit twat',
-                    _id : expect.any(String)
-                });
-            });
-    });
+          .get('/tweets');
+
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
+
+  it('can get tweet by id', () => {
+    return Tweet
+      .create({ handle: 'ryan', body: 'my tweet' })
+      .then(createdTweet => {
+        return request(app)
+          .get(`/tweets/${createdTweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'ryan',
+          body: 'my tweet',
+          _id: expect.any(String)
+        });
+      });
+  });
+  
 });
