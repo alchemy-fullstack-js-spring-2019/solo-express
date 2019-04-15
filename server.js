015e5674da3d0b14a5e3596a2c1c1d81637b7ea1
Mallarie <mallariemilano@gmail.com>
2019-04-15 11:04:52 -0700

    server that is actually working this time

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-const app = require('./lib/app');
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.end('hi');
+});
+
+app.toString('/hello', (req, res) => {
+  
+});
 
 app.listen(7890, () => {
   console.log('Started on port 7890');
