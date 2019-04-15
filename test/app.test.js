e2c5fad64f117477fe31d280b20f30e890c75697
Cara Ponzini <ponzini@gmail.com>
2019-04-15 10:11:44 -0700

    cleaning up practice files

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,13 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const Tweet = require('../lib/models/Tweet');
+
 
 describe('app routes', () => {
+  afterEach(() => {
+    return Tweet.drop();
+  });
+
   it('can create a new tweet', () => {
     return request(app)
       .post('/tweets')
@@ -13,6 +19,34 @@ describe('app routes', () => {
         expect(res.body).toEqual({
           handle: 'cara',
           text: 'my tweet',
+          _id: expect.any(String)
+        });
+      });
+  });
+
+  it('can return all tweets', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res).toEqual([]);
+      });
+  });
+
+  it('can get a tweet by id', () => {
+    return request(app)
+      .post('/tweets')
+      .send({
+        handle: 'test',
+        text: 'test'
+      })
+      .then(tweet => {
+        return request(app)
+          .get(`/${tweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'test',
+          text: 'test',
           _id: expect.any(String)
         });
       });
