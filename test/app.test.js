95025add3847c6be76dd39f570560b043b70c1dd
mcnadams <mcneilbo@oregonstate.edu>
2019-04-15 09:17:39 -0700

    set up

---
+++ test/app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+
+describe('app routes', () => {
+    it('responds with hi at /hello', () => {
+        return request(app)
+            .get('/hello')
+            .then(res => {
+                expect(res.body).toEqual({ name: 'bonnie' });
+            });
+    });
+});
