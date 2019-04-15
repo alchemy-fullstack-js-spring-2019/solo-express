967fd74df6c3042c838e851db6658e0b2ec872dc
Cara Ponzini <ponzini@gmail.com>
2019-04-14 17:56:51 -0700

    created server by requiring app

--- server-practice.js
+++ server-practice.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.listen(7890);
+app.listen(1234);
 
 app.get('/hello', (req, res) => {
   res.send('hi');
--- server.js
+++ server.js
@@ -1,2 +1,4 @@
 const app = require('./lib/app');
 
+app.listen(7890);
+
