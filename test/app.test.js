ebda86a8bc2d2f3934a63b749aa52d6e61320b2d
emily baier <emilyrbaier@gmail.com>
2019-04-15 10:49:53 -0700

    added afterEach drop for tests. also will get list of tweets

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,12 @@
 const { app } = require('../lib/app');
 const request = require('supertest');
+const Tweet = require('../lib/models/Tweet');
 
 describe('app tests', ()=> {
+    afterEach(()=> {
+        return Tweet.drop();
+    });
+
     it('will post a tweet to data', ()=> {
         return request(app)
             .post('/tweets')
@@ -16,5 +21,20 @@ describe('app tests', ()=> {
                     _id: expect.any(String)
                 });
             });
+    });
+
+    it('can get a list of tweets', ()=> {
+        return Tweet
+            .create({
+                handle: 'emily', body: 'my cool tweet' })
+            .then(()=> {
+                return request(app)
+                    .get('/tweets');
+            })
+            .then(res => {
+                expect(res.body).toHaveLength(1);
+            });
+        
+
     });
 });
