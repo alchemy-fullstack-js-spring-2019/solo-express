d8e03e2bd6c400c43d9f2e24dafdbad9be77f015
Emily Creamer <emily.creamer@icloud.com>
2019-04-16 09:52:53 -0700

    passing tests

--- test/app.test.js
+++ test/app.test.js
@@ -7,7 +7,7 @@ describe('app routes', () => {
   beforeAll(done => {
     mkdirp('./data/tweets', done);
   });
-  
+
   afterEach(() => {
     Tweet.drop();
   });
@@ -29,63 +29,66 @@ describe('app routes', () => {
       });
   });
 
-  // it('can get a list of tweets', () => {
-  //   return Tweet
-  //     .create({ handle: 'emily', body: 'another test' })
-  //     .then(() => {
-  //       return request(app)
-  //         .get('/tweets');
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toHaveLength(1);
-  //     });
-  // });
+  it('can get a list of tweets', () => {
+    return Tweet
+      .create({ handle: 'emily', body: 'another test' })
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
 
-  // it('can get a tweet by id', () => {
-  //   return Tweet
-  //     .create({ handle: 'emily', body: 'wow, another test' })
-  //     .then(createdTweet => {
-  //       return request(app)
-  //         .get(`/tweets/${createdTweet._id}`);
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         handle: 'emily',
-  //         body: 'wow, another test',
-  //         _id: expect.any(String)
-  //       });
-  //     });
-  // });
+  it('can get a tweet by id', () => {
+    return Tweet
+      .create({ handle: 'emily', body: 'wow, another test' })
+      .then(createdTweet => {
+        return request(app)
+          .get(`/tweets/${createdTweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'emily',
+          body: 'wow, another test',
+          _id: expect.any(String)
+        });
+      });
+  });
 
-  // it('can updated a tweet by id', () => {
-  //   return Tweet.create({ handle: 'emily', body: 'yet another test' })
-  //     .then(tweet => {
-  //       return request(app)
-  //         .put(`/tweets/${tweet._id}`)
-  //         .send({
-  //           handle: 'emily',
-  //           body: 'yet another test'
-  //         });
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         handle: 'emily',
-  //         body: 'yet another test',
-  //         _id: expect.any(String)
-  //       });
-  //     });
-  // });
+  it('can updated a tweet by id', () => {
+    return Tweet.create({ handle: 'emily', body: 'yet another test' })
+      .then(tweet => {
+        return request(app)
+          .put(`/tweets/${tweet._id}`)
+          .send({
+            handle: 'emily',
+            body: 'yet another test'
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'emily',
+          body: 'yet another test',
+          _id: expect.any(String)
+        });
+      });
+  });
 
-  // it('can delete a tweet by id', () => {
-  //   return Tweet.create({ handle: 'emily', body: 'tests forever' })
-  //     .then(tweet => {
-  //       return request(app)
-  //         .delete(`/tweets/${tweet._id}`);
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         deleted: 1
-  //       });
-  //     });
-  // });
+  it('can delete a tweet by id', () => {
+    return Tweet.create({
+      handle: 'emily',
+      body: 'tests forever'
+    })
+      .then(tweet => {
+        return request(app)
+          .delete(`/tweets/${tweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          deleted: 1
+        });
+      });
+  });
 });
