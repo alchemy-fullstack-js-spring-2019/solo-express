66e6dd946f64217894f4e8b18198116fbd4d8151
Masmics <cherlocker3113@gmail.com>
2019-04-16 19:34:37 -0700

    all tests passing, indentation corrected

--- test/app.test.js
+++ test/app.test.js
@@ -9,81 +9,81 @@ describe('app routes', () => {
   });
 
   //it's CRUD
-  // it('can return a new tweet', () => {
-  //   return request(app)
-  //     .post('/tweets') // update at the url route
-  //     .send({ // send json object (the tweet)
-  //       handle: 'chris',
-  //       body: 'my first tweet'
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         handle: 'chris',
-  //         body: 'my first tweet',
-  //         _id: expect.any(String) //<- magic
-  //       });
-  //     });
-  // });
+  it('can return a new tweet', () => {
+    return request(app)
+      .post('/tweets') // update at the url route
+      .send({ // send json object (the tweet)
+        handle: 'chris',
+        body: 'my first tweet'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'chris',
+          body: 'my first tweet',
+          _id: expect.any(String) //<- magic
+        });
+      });
+  });
 
-  // it('can get a list of tweets', () => {
-  //   return Tweet
-  //     .create({ handle: 'chris', body: 'my tweet' })
-  //     .then(() => {
-  //       return request(app)
-  //         .get('/tweets');
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toHaveLength(1);
-  //     });
-  // });
+  it('can get a list of tweets', () => {
+    return Tweet
+      .create({ handle: 'chris', body: 'my tweet' })
+      .then(() => {
+        return request(app)
+          .get('/tweets');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
 
-  // it('can get a tweet by id', () => {
-  //   return Tweet
-  //     .create({ handle: 'chris', body: 'my tweet' })
-  //     .then(createdTweet => {
-  //       return request(app)
-  //         .get(`/tweets/${createdTweet._id}`);
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         handle: 'chris',
-  //         body: 'my tweet',
-  //         _id: expect.any(String)
-  //       });
-  //     });
-  // });
+  it('can get a tweet by id', () => {
+    return Tweet
+      .create({ handle: 'chris', body: 'my tweet' })
+      .then(createdTweet => {
+        return request(app)
+          .get(`/tweets/${createdTweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'chris',
+          body: 'my tweet',
+          _id: expect.any(String)
+        });
+      });
+  });
 
-  // it('can update a tweet by id', () => {
-  //   return Tweet.create({ handle: 'chris', body: 'my tweet' })
-  //     .then(tweet => {
-  //       return request(app)
-  //         .put(`/tweets/${tweet._id}`)
-  //         .send({
-  //           handle: 'chris',
-  //           body: 'my new tweet'
-  //         });
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         handle: 'chris',
-  //         body: 'my new tweet',
-  //         _id: expect.any(String)
-  //       });
-  //  });
-  // });
+  it('can update a tweet by id', () => {
+    return Tweet.create({ handle: 'chris', body: 'my tweet' })
+      .then(tweet => {
+        return request(app)
+          .put(`/tweets/${tweet._id}`)
+          .send({
+            handle: 'chris',
+            body: 'my new tweet'
+          });
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          handle: 'chris',
+          body: 'my new tweet',
+          _id: expect.any(String)
+        });
+      });
+  });
 
-  // it('can delete a tweet by id', () => {
-  //   return Tweet.create({ handle: 'chris', body: 'delete this tweet' })
-  //     .then(tweet => {
-  //       return request(app)
-  //         .delete(`/tweets/${tweet._id}`);
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         deleted: 1
-  //       });
-  //     });
-  // });
+  it('can delete a tweet by id', () => {
+    return Tweet.create({ handle: 'chris', body: 'delete this tweet' })
+      .then(tweet => {
+        return request(app)
+          .delete(`/tweets/${tweet._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          deleted: 1
+        });
+      });
+  });
 });
 
 describe('tags resource', () => {
@@ -91,46 +91,46 @@ describe('tags resource', () => {
     return Tag.drop();
   });
 
-  // it('can return a new tag', () => {
-  //   return request(app)
-  //     .post('/tags')
-  //     .send({
-  //       name: 'recreation'
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         name: 'recreation',
-  //         _id: expect.any(String)
-  //       });
-  //     });
-  // });
+  it('can return a new tag', () => {
+    return request(app)
+      .post('/tags')
+      .send({
+        name: 'recreation'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'recreation',
+          _id: expect.any(String)
+        });
+      });
+  });
 
-  // it('can get a list of tags', () => {
-  //   return Tag
-  //     .create({ name: 'work' })
-  //     .then(() => {
-  //       return request(app)
-  //         .get('/tags');
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toHaveLength(1);
-  //     });
-  // });
+  it('can get a list of tags', () => {
+    return Tag
+      .create({ name: 'work' })
+      .then(() => {
+        return request(app)
+          .get('/tags');
+      })
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+      });
+  });
 
-  // it('can get a tag by id', () => {
-  //   return Tag
-  //     .create({ name: 'YouGotMeByMyId!', _id: 'mockID' })
-  //     .then(createdTag => {
-  //       return request(app)
-  //         .get(`/tags/${createdTag._id}`);
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         name: 'YouGotMeByMyId!',
-  //         _id: expect.any(String)
-  //       });
-  //     });
-  // });
+  it('can get a tag by id', () => {
+    return Tag
+      .create({ name: 'YouGotMeByMyId!', _id: 'mockID' })
+      .then(createdTag => {
+        return request(app)
+          .get(`/tags/${createdTag._id}`);
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'YouGotMeByMyId!',
+          _id: expect.any(String)
+        });
+      });
+  });
 
   it('can update a tag by ID', () => {
     return Tag.create({ name: 'testMeName' })
