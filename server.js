e09ce1423d875bbd0b210fb7a8e4392537cefc8e
dreamer <wildetangeant@gmail.com>
2019-04-21 15:13:09 -0700

    package.json tweak

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.listen(7890, () => {
-  console.log('started on port 7890');
+app.listen(3000, () => {
+  console.log('started on port 3000');
 });
